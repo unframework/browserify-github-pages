b6399e638f1e48cc1fef3921e38310e2da57e72f
TravisCI Auto-publisher <travis-auto-publish@example.com>
2017-02-02 04:45:45 +0000

    Auto-publishing via TravisCI [skip ci]

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 window.onload = function () {
     document.body.style.textAlign = 'center';
     document.body.style.fontSize = '32pt';
@@ -12,3 +13,5 @@ window.onload = function () {
     link.appendChild(document.createTextNode('See deployment quickstart instructions'));
     document.body.appendChild(link);
 };
+
+},{}]},{},[1]);
